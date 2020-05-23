5033eef5f283eeb667d62fe7a8ab3c00669bd4f2
Kalhara Perera <36784239+Adore96@users.noreply.github.com>
2020-05-23 23:24:31 +0530

    Update SearchFruits.py

--- SearchFruits.py
+++ SearchFruits.py
@@ -1,7 +1,7 @@
 
 from sklearn import tree
 
-# smooth = 1 and bumpy =0
+# smooth = 1 and bumpy = 0
 features = [[140, 1], [130, 1], [150, 0], [170, 0],
             [141, 1], [143, 1], [145, 1], [146, 0]]
 
@@ -18,6 +18,5 @@ adoreclassifier = adoreclassifier.fit(features, labels)
 prediction = adoreclassifier.predict([[142, 1]])
 
 # giving clear outputs to identify here
-
 
 print()
