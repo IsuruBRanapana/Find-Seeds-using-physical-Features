3acdee7501dceccb68340bcaa40a4ac490d32616
IsuruBRanapana <raaisurub.ranapana@gmail.com>
2020-10-26 15:43:34 +0530

    Updated

--- SearchFruits.py
+++ SearchFruits.py
@@ -5,10 +5,11 @@ import numpy
 
 # smooth = 1 and bumpy = 0
 features = [[140, 1], [130, 1], [150, 0], [170, 0],
+            [141, 1], [143, 1], [145, 1], [146, 0],
             [141, 1], [143, 1], [145, 1], [146, 0]]
 
 # apple = 0 and oranges = 1
-labels = [0, 0, 1, 1, 0, 0, 0, 1]
+labels = [0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1]
 
 # making the custom classifier
 adoreclassifier = tree.DecisionTreeClassifier()
